4055b71687438863f936c332dbe7e5d3e5bb1d11
kartikk23 <kartikkhobragade88@gmail.com>
2022-08-20 13:42:55 +0530

    Initial Commit

---
+++ app/src/main/java/com/kartik/retro/myapi.java
@@ -0,0 +1,12 @@
+package com.kartik.retro;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface myapi {
+    @GET("posts")
+    Call<List<model>> getmodel();
+}
+
